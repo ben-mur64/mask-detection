b1f7cdec96d8e1e2a63ea0cbe3890077f8186018
Ben Murray <murraybp64@gmail.com>
2020-11-16 11:42:51 -0700

    Saved a model

--- main.py
+++ main.py
@@ -5,13 +5,69 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
 from keras.applications import MobileNetV2
-from keras.models import Model
+from keras.models import Model, save_model, load_model
 from keras.layers import Input, Dense, Activation, SeparableConv2D, MaxPooling2D, Flatten, BatchNormalization, Dropout
 from keras.optimizers import Adam
 
 batch_size = 40
 img_height = 200
 img_width = 200
+
+BUILDING = False
+LOADING = True
+
+def build_model(img_height, img_width):
+    inputs = Input(shape=(img_height, img_width, 3))
+
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    activation = "sigmoid"
+    units = 1
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+
+    model = Model(inputs=inputs, outputs=outputs)
+    print(model.summary())
+
+    model.compile(optimizer=Adam(1e-3), loss='binary_crossentropy', metrics=['accuracy'])
+    model.fit(training_ds, epochs=6, batch_size=32, validation_data=testing_ds)
+    save_model(model, "./best_model")
+
+def load():
+    model = load_model('best_model')
+    return model
 
 
 # Keras dataset preprocessing utilities, located at tf.keras.preprocessing,
@@ -60,88 +116,16 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 training_ds = training_ds.cache().prefetch(buffer_size=AUTOTUNE)
 testing_ds = testing_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
+if BUILDING:
+    build_model(img_height, img_width)
+
+if LOADING:
+    model = load()
+
+predict = model.predict(testing_ds)
+true = np.concatenate([y for x,y in testing_ds], axis=0)
+con_mat = tf.math.confusion_matrix(labels=true, predictions=predict)
+print(con_mat)
 
 # # Now build a deep neural network and train it and see how you do
 
-# 
-inputs = Input(shape=(img_height, img_width, 3))
-
-x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
-x = layers.BatchNormalization()(x)
-x = layers.Activation("relu")(x)
-
-x = layers.Conv2D(64, 3, padding="same")(x)
-x = layers.BatchNormalization()(x)
-x = layers.Activation("relu")(x)
-
-previous_block_activation = x  # Set aside residual
-
-for size in [128, 256, 512, 728]:
-    x = layers.Activation("relu")(x)
-    x = layers.SeparableConv2D(size, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-
-    x = layers.Activation("relu")(x)
-    x = layers.SeparableConv2D(size, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-
-    x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-    # Project residual
-    residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-        previous_block_activation
-    )
-    x = layers.add([x, residual])  # Add back residual
-    previous_block_activation = x  # Set aside next residual
-
-x = layers.SeparableConv2D(1024, 3, padding="same")(x)
-x = layers.BatchNormalization()(x)
-x = layers.Activation("relu")(x)
-
-x = layers.GlobalAveragePooling2D()(x)
-activation = "sigmoid"
-units = 1
-
-x = layers.Dropout(0.5)(x)
-outputs = layers.Dense(units, activation=activation)(x)
-
-model = Model(inputs=inputs, outputs=outputs)
-print(model.summary())
-# 2 Convolutional layers each followed by max pooling
-# x = SeparableConv2D(32, kernel_size=(5, 5), kernel_initializer='he_normal')(inputs)
-# x = BatchNormalization()(x)
-# x = Activation('relu')(x)
-# x = MaxPooling2D(pool_size=(2, 2))(x)
-
-# x = SeparableConv2D(64, kernel_size=(3, 3), activation='relu', kernel_initializer='he_normal')(x)
-# x = BatchNormalization()(x)
-# x = Activation('relu')(x)
-# x = MaxPooling2D(pool_size=(2, 2))(x)
-
-# x = SeparableConv2D(128, kernel_size=(3, 3), activation='relu', kernel_initializer='he_normal')(x)
-# x = BatchNormalization()(x)
-# x = Activation('relu')(x)
-# x = MaxPooling2D(pool_size=(2, 2))(x)
-
-# x = SeparableConv2D(256, kernel_size=(3, 3), activation='relu', kernel_initializer='he_normal')(x)
-# x = BatchNormalization()(x)
-# x = Activation('relu')(x)
-# x = MaxPooling2D(pool_size=(2, 2))(x)
-
-# x = Flatten()(x)
-# x = Dense(128, activation='relu', kernel_initializer='he_normal')(x)
-# x = Dropout(0.5)(x)
-# x = Dense(2, activation='softmax')(x)
-
-# Different go at it
-# base = MobileNetV2(weights="imagenet", include_top=False, input_tensor=inputs)
-
-# x = base.output
-# x = MaxPooling2D(pool_size=(7,7))(x)
-# x = Flatten(name="flatten")(x)
-# x = Dense(128, activation='relu')(x)
-# x = Dropout(0.5)(x)
-# x = Dense(2, activation='softmax')(x)
-
-model.compile(optimizer=Adam(1e-3), loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(training_ds, epochs=6, batch_size=32, validation_data=testing_ds)
